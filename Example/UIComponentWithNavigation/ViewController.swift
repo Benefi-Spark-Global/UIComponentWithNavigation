e93c748c0a93050816b12e485af7cbc4017daf23
punitbenefi <punit@benefi.org>
2024-11-10 18:53:35 +0530

    First Commit
    Test Commit

--- Example/UIComponentWithNavigation/ViewController.swift
+++ Example/UIComponentWithNavigation/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import UIComponentWithNavigation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let abc = Logger()
+        abc.printlog("kya haal hai vishal")
+
     }
 
     override func didReceiveMemoryWarning() {
